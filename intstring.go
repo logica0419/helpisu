ba09fbf3d826dd32eec201ca6ab2b838709b3d37
logica <84759675+logica0419@users.noreply.github.com>
2022-06-24 19:07:58 +0900

    added IntString

---
+++ intstring.go
@@ -0,0 +1,68 @@
+package helpisu
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+)
+
+/*
+StringInt DBにはint型に、jsonにはstring型として認識される特殊な型
+
+PrimaryKeyをランダムなstringからauto incrementなintに変換する時などに使います
+中身の値を使いたいときは、Value()メソッドを使用して下さい
+*/
+type StringInt struct {
+	value int
+}
+
+// ErrInvalidType 不正な型が渡されたときに返すエラー
+var ErrInvalidType = errors.New("StringInt.Scan: invalid type")
+
+// NewStringInt 新しいStringIntを作成
+func NewStringInt(value int) StringInt {
+	return StringInt{
+		value: value,
+	}
+}
+
+// UnmarshalJSON json.Unmarshalerの実装
+func (si *StringInt) UnmarshalJSON(data []byte) error {
+	var str string
+
+	err := json.Unmarshal(data, &str)
+	if err != nil {
+		return err
+	}
+
+	si.value, err = strconv.Atoi(str)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// MarshalJSON json.Marshalerの実装
+func (si StringInt) MarshalJSON() ([]byte, error) {
+	str := strconv.Itoa(si.value)
+
+	return json.Marshal(str)
+}
+
+// Scan sql.Scannerの実装
+func (si *StringInt) Scan(value any) error {
+	var ok bool
+
+	si.value, ok = value.(int)
+	if !ok {
+		return ErrInvalidType
+	}
+
+	return nil
+}
+
+// Value sql.Valuerの実装
+func (si StringInt) Value() int {
+	return si.value
+}
