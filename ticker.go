79830daa0ff995c8cc14e10905b4d8d822fd605a
logica <84759675+logica0419@users.noreply.github.com>
2022-07-21 06:41:23 +0900

    implemented Ticker

---
+++ ticker.go
@@ -0,0 +1,51 @@
+package helpisu
+
+import "time"
+
+// Ticker 一定時間ごとに関数を実行するタイマー
+type Ticker struct {
+	d time.Duration
+	t *time.Ticker
+	f func()
+	s chan struct{}
+}
+
+/*
+NewTicker 新たなTickerを作成
+	durationSecはタイマーの実行間隔を秒で指定して下さい
+*/
+func NewTicker(durationSec int, callback func()) *Ticker {
+	return &Ticker{
+		d: time.Duration(durationSec) * time.Second,
+		f: callback,
+		s: make(chan struct{}),
+	}
+}
+
+// Start タイマーを開始
+func (t *Ticker) Start() {
+	t.t = time.NewTicker(t.d)
+	defer t.t.Stop()
+	defer close(t.s)
+
+	go func() {
+		for {
+			select {
+			case <-t.t.C:
+				go t.f()
+			case <-t.s:
+				return
+			}
+		}
+	}()
+}
+
+// Stop タイマーを停止
+func (t *Ticker) Stop() {
+	t.s <- struct{}{}
+}
+
+// Reset タイマーをリセット
+func (t *Ticker) Reset() {
+	t.t.Reset(t.d)
+}
