b43511d26f5f96fe515d913aba929ee6ea2d6b2e
logica <84759675+logica0419@users.noreply.github.com>
2022-07-23 03:27:52 +0900

    add Pause method to DBDisconnectDetector

--- db.go
+++ db.go
@@ -21,20 +21,26 @@ func WaitDBStartUp(db *sql.DB) {
 // DBDisconnectDetector DBから切断されるとアプリを強制終了する検出器
 type DBDisconnectDetector struct {
 	t *Ticker
+	r time.Duration
+	s chan struct{}
 }
 
 /*
 NewDBDisconnectDetector 新たなDBDisconnectDetectorを作成
 	durationSecは接続確認の実行間隔をs単位で指定して下さい
+	pauseSecは`Pause()`してから検出を再開するまでの時間をs単位で指定して下さい
 */
-func NewDBDisconnectDetector(durationSec int, db *sql.DB) *DBDisconnectDetector {
+func NewDBDisconnectDetector(durationSec, pauseSec int, db *sql.DB) *DBDisconnectDetector {
 	return &DBDisconnectDetector{
 		t: NewTicker(durationSec*1000, func() {
+			log.Println("check DB connection")
 			err := db.Ping()
 			if err != nil {
 				log.Panic("DB disconnected")
 			}
 		}),
+		r: time.Second * time.Duration(pauseSec),
+		s: make(chan struct{}),
 	}
 }
 
@@ -43,12 +49,29 @@ Start DBからの切断の検出を開始
 	必ずGoroutineとして実行して下さい
 */
 func (d *DBDisconnectDetector) Start() {
-	d.t.Start()
+	for {
+		select {
+		case <-d.s:
+			return
+		default:
+			d.t.Start()
+			time.Sleep(d.r)
+		}
+	}
 }
 
-// Stop DBからの切断の検出を停止
+/*
+Pause DBからの切断の検出を一時的に停止
+	検出はpauseSec秒後に再開します
+*/
+func (d *DBDisconnectDetector) Pause() {
+	d.t.Stop()
+}
+
+// Stop DBからの切断の検出を完全に停止
 func (d *DBDisconnectDetector) Stop() {
 	d.t.Stop()
+	d.s <- struct{}{}
 }
 
 // Reset 確認タイミングをリセット
