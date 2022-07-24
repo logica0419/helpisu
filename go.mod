bcdbb00df3d4a60ac52961379eeb46afaa86198e
logica <84759675+logica0419@users.noreply.github.com>
2022-07-24 23:17:00 +0900

    implemented uuid & ulid generator

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.18
 
 require (
 	github.com/bytedance/sonic v1.3.3
+	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.7.2
+	github.com/oklog/ulid/v2 v2.1.0
 	github.com/stretchr/testify v1.8.0
 )
 
---
+++ id.go
@@ -0,0 +1,22 @@
+package helpisu
+
+import (
+	"github.com/google/uuid"
+	"github.com/oklog/ulid/v2"
+)
+
+/*
+NewUUID 新たなUUIDを作成
+	36文字のstring形式で返します
+*/
+func NewUUID() string {
+	return uuid.Must(uuid.NewRandom()).String()
+}
+
+/*
+NewULID 新たなULIDを作成
+	26文字のstring形式で返します
+*/
+func NewULID() string {
+	return ulid.Make().String()
+}
