261a04c8b226892072bc903643dc8a98dd64afe4
logica <84759675+logica0419@users.noreply.github.com>
2022-07-26 03:24:45 +0900

    implemented ResetAllCache

--- cache.go
+++ cache.go
@@ -1,6 +1,14 @@
 package helpisu
 
-import "sync"
+import (
+	"sync"
+)
+
+type resetter interface {
+	Reset()
+}
+
+var generatedCaches = []resetter{}
 
 /*
 Cache ジェネリックで、スレッドセーフなマップキャッシュ
@@ -12,9 +20,13 @@ type Cache[K comparable, V any] struct {
 
 // NewCache 新たなCacheを作成
 func NewCache[K comparable, V any]() *Cache[K, V] {
-	return &Cache[K, V]{
+	c := Cache[K, V]{
 		m: &sync.Map{},
 	}
+
+	generatedCaches = append(generatedCaches, &c)
+
+	return &c
 }
 
 // Get 指定したKeyのキャッシュを取得
@@ -54,4 +66,11 @@ func (c *Cache[K, V]) Delete(key K) {
 // Reset 全てのキャッシュを削除
 func (c *Cache[K, V]) Reset() {
 	c.m = &sync.Map{}
+}
+
+// ResetAllCache `NewCache()`で生成した全てのキャッシュをリセット
+func ResetAllCache() {
+	for _, c := range generatedCaches {
+		c.Reset()
+	}
 }
