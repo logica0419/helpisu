ef5e1a7878fbfec2da2c8ef5f5f24f3d454ef507
logica <84759675+logica0419@users.noreply.github.com>
2023-01-28 04:08:08 +0900

    cut off cache to child package & add member funcs

--- cache/cache.go
+++ cache/cache.go
@@ -1,4 +1,4 @@
-package helpisu
+package cache
 
 import (
 	"sync"
@@ -17,12 +17,18 @@ Cache ジェネリックで、スレッドセーフなマップキャッシュ
 */
 type Cache[K comparable, V any] struct {
 	m *sync.Map
+	s func(key K, value V)
+	d func(key K)
+	r func()
 }
 
-// NewCache 新たなCacheを作成
-func NewCache[K comparable, V any]() *Cache[K, V] {
+// New 新たなCacheを作成
+func New[K comparable, V any]() *Cache[K, V] {
 	c := Cache[K, V]{
 		m: &sync.Map{},
+		s: nil,
+		d: nil,
+		r: nil,
 	}
 
 	generatedCaches = append(generatedCaches, &c)
@@ -57,11 +63,19 @@ func (c *Cache[K, V]) GetAndDelete(key K) (value V, ok bool) {
 // Set 指定したKey-Valueのセットをキャッシュに入れる
 func (c *Cache[K, V]) Set(key K, value V) {
 	c.m.Store(key, value)
+
+	if c.s != nil {
+		c.s(key, value)
+	}
 }
 
 // Delete 指定したKeyのキャッシュを削除
 func (c *Cache[K, V]) Delete(key K) {
 	c.m.Delete(key)
+
+	if c.d != nil {
+		c.d(key)
+	}
 }
 
 // ForEach キャッシュの全ての要素に対して処理を行う
@@ -80,10 +94,14 @@ func (c *Cache[K, V]) ForEach(f func(key K, value V) error) (err error) {
 // Reset 全てのキャッシュを削除
 func (c *Cache[K, V]) Reset() {
 	c.m = &sync.Map{}
+
+	if c.r != nil {
+		c.r()
+	}
 }
 
-// ResetAllCache `NewCache()`で生成した全てのキャッシュをリセット
-func ResetAllCache() {
+// ResetAll `NewCache()`で生成した全てのキャッシュをリセット
+func ResetAll() {
 	for _, c := range generatedCaches {
 		c.Reset()
 	}
---
+++ cache/doc.go
@@ -0,0 +1,4 @@
+/*
+Package cache provides a generic cache for ISUCON.
+*/
+package cache
